63e5934f09973d360bb8c13396e3e50519dfb82d
Made Erwin A <erwin.ardiantha@pegadaian.co.id>
2020-04-26 16:02:26 +0700

    add: function validate. fixing styles"

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,14 +1,16 @@
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button, Columns, Column } from 'react-bulma-components';
 import React, { Component } from 'react'
 import './Counter.css';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button, Container } from 'react-bulma-components';
 
 export default class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 1
-    };
+    this.state = { count: 1 };
+  }
+
+  componentDidMount() {
+    document.title = 'Sapi Counter';
   }
 
   _increment = () => {
@@ -24,21 +26,31 @@ export default class Counter extends Component {
     this.setState({ count: 1 })
   }
 
+  _validate = () => {
+    if (this.state.count !== 8) {
+      alert('Wrong answer, please think again human')
+      this.setState({ count: 1 })
+      return
+    }
+
+    // navigate to wikipedia cow page 
+    alert('Congrats, You\'re right :)')
+    window.location.href = 'https://id.wikipedia.org/wiki/Sapi'
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>How many cows are there?</h2>
-          <img src="https://reuniting-europe.blogactiv.eu/files/2016/02/Cows-1.jpg" className="sapi-img" alt="sapi" />
-          <Button onClick={() => this._decrement()}> - </Button>
-          <p>{this.state.count} </p>
-          <Button onClick={() => this._increment()}> + </Button>
-          <Button color="primary">Validate</Button>
-        </header>
-        <body>
-          <Container>
-
-          </Container>
+        <body className="App-header">
+          <h2> <b>How many cows are there? </b></h2>
+          <img src="https://reuniting-europe.blogactiv.eu/files/2016/02/Cows-1.jpg" alt="sapi" /><br />
+          <Columns>
+            <Columns.Column> <Button onClick={() => this._decrement()}> - </Button> </Columns.Column>
+            <Columns.Column> {this.state.count} </Columns.Column>
+            <Columns.Column> <Button onClick={() => this._increment()}> + </Button><br /> </Columns.Column>
+          </Columns>
+          <Button color="link" onClick={() => this._validate()}>Validate</Button>
         </body>
       </div>
     );
