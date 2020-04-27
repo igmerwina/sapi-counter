8dd542f8fea4dbd2c9a62f7e4903fd525ba2597f
I Gusti Made Erwin A <igmerwina@gmail.com>
2020-04-27 19:53:49 +0700

    Updates

---
+++ precache-manifest.3d2adfdf44648ae13e2d900225a62059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c543ba0bd7e6079160b59d00d33a8d33",
+    "url": "/sapi-counter/index.html"
+  },
+  {
+    "revision": "533863aae5c4fe6f44d0",
+    "url": "/sapi-counter/static/css/2.fe8af591.chunk.css"
+  },
+  {
+    "revision": "36a018e63608b2e50d77",
+    "url": "/sapi-counter/static/css/main.49f711a2.chunk.css"
+  },
+  {
+    "revision": "533863aae5c4fe6f44d0",
+    "url": "/sapi-counter/static/js/2.ce4aecb0.chunk.js"
+  },
+  {
+    "revision": "e01eb0f37af85dfcc8900424d5ca88ea",
+    "url": "/sapi-counter/static/js/2.ce4aecb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a018e63608b2e50d77",
+    "url": "/sapi-counter/static/js/main.84eb6122.chunk.js"
+  },
+  {
+    "revision": "d94cfa21c8d4154b5a9b",
+    "url": "/sapi-counter/static/js/runtime-main.7210031a.js"
+  }
+]);
